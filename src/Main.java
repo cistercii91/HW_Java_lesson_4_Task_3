cb784b80dc01dd6c4a4188fe37764effc061c5bb
cistercii <cistercii@mail.ru>
2023-03-05 23:59:30 +0300

    исправил расчет месечного процента. Заменил переменную кол. месяцев на значение 12.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         int capital = 1000000;
         double annualRate = 9.99;
-        int year = 12;
-        double monthlyRate = (annualRate / year)/100;
+        int year = 36;
+        double monthlyRate = (annualRate / 12)/100;
         int result = service.calculate(capital, monthlyRate, year);
         System.out.println("Ваша месячная плата составляет " + result);
 
